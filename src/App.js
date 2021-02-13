1bfb0946684673d63a7f9f13fe2b7aaf89ad042d
René Don <renedon@outlook.com>
2021-02-13 17:58:35 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
 		<p className="App-intro">
-			Docker and Kubernetes: The Complete Guide 
+			Docker and Kubernetes: The Complete Guide XXXXXXXXXXXX
 		</p>
         <a
           className="App-link"
